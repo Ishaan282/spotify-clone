a744ecfe54244e63fff905ed6e787ec6b7cb71b0
Ishaan1106 <shunsuke062004@gmail.com>
2024-02-21 18:51:53 +0000

    Feb 22, 2024, 12:21 AM

--- ishaan/main/smort.js
+++ ishaan/main/smort.js
@@ -1,14 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var imgElement = document.querySelector('.bottom .middle .up .play img');
-    var img1 = './main/img/icon/icons8-play-96.png';
-    var img2 = './main/img/icon/icons8-pause-60.png';
-
-    imgElement.addEventListener('click', function() {
-        if (this.src.endsWith(img1)) {
-            this.src = img2;
-        } else {
-            this.src = img1;
-        }
+    document.querySelector('.bottom .middle .up .play img').addEventListener('click', function() {
+        this.src = './main/img/icon/icons8-pause-60.png';
     });
 });
 
