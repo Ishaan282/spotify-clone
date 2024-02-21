72ecbde27b922187fa34941925e246efc98cc0e1
Ishaan1106 <shunsuke062004@gmail.com>
2024-02-21 19:37:06 +0000

    Feb 22, 2024, 1:07 AM

--- ishaan/main/smort.js
+++ ishaan/main/smort.js
@@ -1,22 +1,33 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var playButton = document.querySelector('.bottom .middle .up .play img');
-    playButton.addEventListener('click', function() {
-        if (this.src.endsWith('icons8-play-96.png')) {
-            this.src = './main/img/icon/icons8-pause-60.png';
-        } else {
-            this.src = './main/img/icon/icons8-play-96.png';
-        }
-    });
-});
+// document.addEventListener('DOMContentLoaded', function() {
+//     var playButton = document.querySelector('.bottom .middle .up .play img');
+//     playButton.addEventListener('click', function() {
+//         if (this.src.endsWith('icons8-play-96.png')) {
+//             this.src = './main/img/icon/icons8-pause-60.png';
+//         } else {
+//             this.src = './main/img/icon/icons8-play-96.png';
+//         }
+//     });
+// });
 
 console.log('smort.js loaded');
 
 
 
-document.querySelector('.play').addEventListener('click', function() {
-    var input = document.querySelector('.right input');
-    var currentValue = parseInt(input.value, 10);
-    currentValue += 10; // Change this value to control how much the input should move forward
-    if (currentValue > 100) currentValue = 100; // Ensure the value doesn't exceed the maximum
-    input.value = currentValue;
+document.addEventListener('DOMContentLoaded', function() {
+    var playButton = document.querySelector('.bottom .middle .up .play');
+    var playButtonImg = playButton.querySelector('img');
+    var rightInput = document.querySelector('.down input');
+
+    playButton.addEventListener('click', function() {
+        if (playButtonImg.src.endsWith('icons8-play-96.png')) {
+            playButtonImg.src = './main/img/icon/icons8-pause-60.png';
+        } else {
+            playButtonImg.src = './main/img/icon/icons8-play-96.png';
+        }
+
+        var currentValue = parseInt(rightInput.value, 10);
+        currentValue += 10; // Change this value to control how much the input should move forward
+        if (currentValue > 100) currentValue = 100; // Ensure the value doesn't exceed the maximum
+        rightInput.value = currentValue;
+    });
 });
