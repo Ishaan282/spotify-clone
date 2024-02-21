b5d90e85f24f1e11697e775ea10fafb450b1e331
Ishaan1106 <shunsuke062004@gmail.com>
2024-02-21 18:45:32 +0000

    Feb 22, 2024, 12:15 AM

---
+++ ishaan/main/smort.js
@@ -0,0 +1,3 @@
+document.querySelector('.bottom .middle .up .play img').addEventListener('click', function() {
+    this.src = './main/img/icon/icons8-pause-60.png'; // replace with the path to your new image
+});
